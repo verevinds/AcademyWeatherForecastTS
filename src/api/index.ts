b225e689760853099f6d7998dd033cea4e9212a9
Denis Verevin <verevin@mercurydevelopment.com>
2022-02-11 10:33:21 +0700

    Add type to import

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
-import axios, {AxiosInstance} from 'axios';
+import axios from 'axios';
+import type {AxiosInstance} from 'axios';
 import {API_BASE, API_KEY_TOKEN} from 'react-native-dotenv';
 
 export const WeatherApi: AxiosInstance = axios.create({
